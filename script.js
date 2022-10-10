cbecfa2585f63c217941e8555ae844f5bc3dc5ec
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-10-10 18:14:51 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ const form = document.querySelector('#form')
 const input = document.querySelector('#input')
 const todosUl = document.querySelector('#todos')
 
+const todos = JSON.parse(localStorage.getItem('todos'))
+
+if(todos) {
+  todos.forEach(todo => {
+    addTodo(todo)
+  });
+}
+
 form.addEventListener('submit', (e)=> {
   e.preventDefault()
 
@@ -23,15 +31,44 @@ function addTodo(todo) {
 
     todoEl.innerText = todoText
 
-    todoEl.addEventListener('click', ()=> todoEl.classList.toggle('completed'))
+    todoEl.addEventListener('click', ()=> {
+      todoEl.classList.toggle('completed')
+      updateLocalStorage()
+    })
 
     todoEl.addEventListener('contextmenu', (e)=> {
       e.preventDefault()
       todoEl.remove()
+      updateLocalStorage()
     })
 
     todosUl.appendChild(todoEl)
 
     input.value = ''
+
+    updateLocalStorage()
   }
 }
+
+function updateLocalStorage() {
+  todosEl = document.querySelectorAll('li')
+  
+  const todos = []
+
+  todosEl.forEach(todoEl => {
+    todos.push({
+    text: todoEl.innerText,
+    completed: todoEl.classList.contains('completed')
+  })
+  })
+
+  localStorage.setItem('todos', JSON.stringify(todos))
+}
+
+
+//To Use Local Storage:
+/*
+localStorage.setItem() // add item to the storage
+localStorage.getItem() // buscar item from the storage
+localStorage.removeItem() // remove item from the storage
+*/
