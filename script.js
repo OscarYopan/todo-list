14d414351b99efc1266efabc36a81264d9f8430d
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-10-10 17:32:05 -0300

    SCRIPT

---
+++ script.js
@@ -0,0 +1,37 @@
+const form = document.querySelector('#form')
+const input = document.querySelector('#input')
+const todosUl = document.querySelector('#todos')
+
+form.addEventListener('submit', (e)=> {
+  e.preventDefault()
+
+  addTodo()
+})
+
+function addTodo(todo) {
+  let todoText = input.value
+
+  if(todo) {
+    todoText = todo.text
+  }
+
+  if(todoText) {
+    const todoEl = document.createElement('li')
+    if(todo && todo.completed) {
+      todoEl.classList.add('completed')
+    }
+
+    todoEl.innerText = todoText
+
+    todoEl.addEventListener('click', ()=> todoEl.classList.toggle('completed'))
+
+    todoEl.addEventListener('contextmenu', (e)=> {
+      e.preventDefault()
+      todoEl.remove()
+    })
+
+    todosUl.appendChild(todoEl)
+
+    input.value = ''
+  }
+}
